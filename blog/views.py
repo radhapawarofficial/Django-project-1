8b2a0cb37d6a18ae13224bee2ba78184eb003e51
radhapawarofficial <radhapawarofficial@gmail.com>
2026-02-05 21:20:37 +0530

    Added Kotlin FAQ's 1-50 Gists in docsallover

--- blog/views.py
+++ blog/views.py
@@ -11,15 +11,6 @@ class PostListView(ListView):
 
 def qa_view(request):
     return render(request, 'blog/. from django.views.generic import ListView')
-from django.shortcuts import render
-from .models import Post
-
-
-class PostListView(ListView):
-    model = Post
-    template_name = 'blog/post_list.html'
-    context_object_name = 'all_posts'
-
 
 def django-faq_view(request):
     return render(request, 'blog/django-faq.html')')
